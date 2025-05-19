abaf4ebccf55d57207f2dde3706357b7131307d6
Selam Anderson <Selam248@gmail.com>
2025-05-19 08:57:48 -0500

    deployment

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 import Banner from "../../components/Banner/Banner";
+import RowList from "../../components/Rows/RowList/RowList";
 
 function Home() {
   return (
     <>
       <Header />
       <Banner />
+      <RowList />
       <Footer />
     </>
   );
